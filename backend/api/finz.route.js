c6390cde253e224f1be32d7c74e18d7cae7626e5
Knight <77844703+nightcoder26@users.noreply.github.com>
2023-11-06 15:06:39 +0530

    delete

--- backend/api/finz.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.route("/").get((req, res) => res.send("Hello World!"));
-router.route("/home").get((req, res) => res.send("Hello Home!"));
-
-export default router;
