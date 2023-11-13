2ec3d2b39ea1b07e2dac7301a349a78c3cbc62a0
Knight <77844703+nightcoder26@users.noreply.github.com>
2023-11-13 22:16:41 +0530

    comments

--- frontend2/src/pages/Home.jsx
+++ frontend2/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Navbar from "../components/Navbar.jsx";
 const Home = () => {
   return (
     <div>
+      {/* <Sidebar/> sidebar goes here 
+          each page with 3 components a sidebar, navbar and main content, main content has other components
+      */}
       <Navbar pageName="Overview" />
     </div>
   );
