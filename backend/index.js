6adda6be41b9a3d353ff1b23505678680ba9e7cd
Knight <77844703+nightcoder26@users.noreply.github.com>
2023-11-14 01:21:59 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
+const cors = require("cors");
+
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const { connectDatabase } = require("./db/connection.js");
 const homeRoute = require("./routes/homeRoute.js");
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
+app.use(cors());
+
 app.use("/api", homeRoute);
 app.listen(PORT, () => {
   connectDatabase();
