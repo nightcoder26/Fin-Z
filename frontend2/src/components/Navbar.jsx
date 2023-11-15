0047a8463f340b7ea59dbdac5510a8bc4590e0c3
bloodwolf-debug <dineshsathvik@gmail.com>
2023-11-15 22:39:36 +0530

    navbar

--- frontend2/src/components/Navbar.jsx
+++ frontend2/src/components/Navbar.jsx
@@ -2,19 +2,11 @@ import React from "react";
 import "../styles/Navbar.css";
 const Navbar = (props) => {
   return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-div">
-          <h1>{props.pageName}</h1>
-
-          <img
-            src="https://www.freeiconspng.com/uploads/empty-gold-coin-icon-19.jpg"
-            alt="Empty Gold Coin Icon"
-            className="logo"
-          />
-        </div>
-      </nav>
-    </div>
+    <nav className="nav">
+      <a href="/" className = "site-title">
+        {props.pageName}
+      </a>
+    </nav>
   );
 };
 
