a9477e3979dad1cd8102451e8003e85c45a62d16
Pravalika <106540790+Pravalka@users.noreply.github.com>
2023-12-12 19:46:53 +0530

    Add files via upload

---
+++ Week.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/datepicker';
+
+function WeekPage() {
+  const [selectedDate, setSelectedDate] = useState('');
+
+  $(function () {
+    $("#datepicker").datepicker({
+      onSelect: function (dateText) {
+        setSelectedDate(dateText);
+      }
+    });
+
+    $(".date-picker").on('click', function () {
+      $("#datepicker").datepicker("show");
+    });
+  });
+
+  return (
+    <div className="main-container">
+      <div className="horizontal-container">
+        <div className="horizontal-box"></div>
+        <div className="horizontal-box"></div>
+        <div className="horizontal-box third-box"></div>
+        <input type="text" className="date-picker" id="datepicker" />
+      </div>
+
+      <div className="vertical-container">
+        {[...Array(10)].map((_, index) => (
+          <div className="vertical-box" key={index}></div>
+        ))}
+      </div>
+
+      <p id="selectedDate">{selectedDate}</p>
+    </div>
+  );
+}
+
+export default WeekPage;
