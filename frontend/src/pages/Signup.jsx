71c33e6697f7aaf4f4da4f27868592d2fc6f3eb0
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-01-27 23:04:27 +0530

    no-cors

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -21,6 +21,7 @@ const SignUp = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ username, password }),
+          mode: "no-cors",
         }
       );
 
