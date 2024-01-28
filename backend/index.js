5bc79e618ab8dab9f1ad317a2d1fe08a83423f00
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-01-28 09:06:37 +0530

    Revert "changed origin"
    This reverts commit 838907df6661ae652f9996e9f6507d959fcd39fa.

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://deploy-fin-z.vercel.app",
+    origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
