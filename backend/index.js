d75528a62783fc8f6af17d047fc22c5cb790108a
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-01-28 09:03:38 +0530

    Revert "cors origin rename"
    This reverts commit bfe1adf14636471c2f66859998ddf9452b8b579e.

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://fin-z.vercel.app",
+    origin: "https://deploy-fin-z.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
