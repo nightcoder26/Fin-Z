0d460ee99b9328758934126563e15439d09ad4b7
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-01-28 09:01:11 +0530

    Revert "cors issues"
    This reverts commit d13629f0736970589e131e6146047549d987cf87.

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://fin-z.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
 
