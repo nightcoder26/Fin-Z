3501a76e76813e40238973f61a18ed80465d78ba
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-01-29 11:34:56 +0530

    d

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://fin-z.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
